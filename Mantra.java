423c8809c9c010ae9b3ebfbc940135f599bf886d
Rattl3bones <42248595+Rattl3bones@users.noreply.github.com>
2019-08-26 18:49:17 -0400

    Exercises 5-9

---
+++ Mantra.java
@@ -0,0 +1,21 @@
+public class Mantra 
+{
+   public static void main(String[] args)
+   {
+      Text();
+      System.out.println("");
+      Text();
+   
+   
+   }
+   public static void Text()
+   {
+      System.out.println("There's one thing every coder must understand:");
+      System.out.println("The System.out.println command.");
+   
+   }
+
+
+
+
+}
