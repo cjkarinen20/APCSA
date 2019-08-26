f13255397c9bf9b916b9aef770c58c0759c5fde9
Cj Karinen <42248595+cjkarinen20@users.noreply.github.com>
2019-08-26 07:48:56 -0400

    Pikachu Program

---
+++ Pikachu.java
@@ -0,0 +1,14 @@
+public class Pikachu   
+{ 
+   public static void main(String[] args) 
+   {
+      System.out.println(" (\\__/)");
+      System.out.println(" (o^.^)");
+      System.out.println("z(_(\")(\")");
+
+   }
+   
+
+
+
+}
