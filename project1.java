1bb6ff0aeecf3b970dff4c1416e4f56a83b41ad1
Rattl3bones <42248595+Rattl3bones@users.noreply.github.com>
2019-09-04 16:26:18 -0400

    Unit 1 Project 1: Mississippi

---
+++ project1.java
@@ -0,0 +1,77 @@
+public class project1 
+{
+   public static void main(String[] args) //Project 1; Mississippi - Cj Karinen
+   {
+      drawM();
+      drawISS();
+      drawISS();
+      drawI();
+      drawP();
+      drawP();
+      drawI();
+   
+   
+   
+   
+   
+   }
+   public static void drawM() 
+   {
+      System.out.println("M     M");
+      System.out.println("MM   MM");
+      System.out.println("M M M M");
+      System.out.println("M  M  M");
+      System.out.println("M     M");
+      System.out.println("M     M");
+      System.out.println("M     M");
+      System.out.println("");
+   }
+   public static void drawP()
+   {
+      System.out.println("PPPPPPP");
+      System.out.println("P     P");
+      System.out.println("P     P");
+      System.out.println("PPPPPPP");
+      System.out.println("P      ");
+      System.out.println("P      ");
+      System.out.println("P      ");
+      System.out.println("");
+   }
+   public static void drawI()
+   {
+      System.out.println(" IIIII");
+      System.out.println("   I  ");
+      System.out.println("   I  ");
+      System.out.println("   I  ");
+      System.out.println("   I  ");
+      System.out.println("   I  ");
+      System.out.println(" IIIII");
+      System.out.println("");
+
+   }
+   public static void drawS()
+   {
+      System.out.println(" SSSSS");
+      System.out.println("S     S");
+      System.out.println("S     ");
+      System.out.println(" SSSSS");
+      System.out.println("      S");
+      System.out.println("S     S");
+      System.out.println(" SSSSS");
+      System.out.println("");
+      
+   }
+   public static void drawISS()
+   {
+      drawI();
+      drawS();
+      drawS();
+      
+   }
+
+
+
+
+
+
+}
