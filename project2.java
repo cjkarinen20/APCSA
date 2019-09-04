10c2026cc975a8f336ab1751e2703b45a327b99b
Rattl3bones <42248595+Rattl3bones@users.noreply.github.com>
2019-09-04 17:44:31 -0400

    Project 2: Old Lady, CJ Karinen

---
+++ project2.java
@@ -0,0 +1,96 @@
+public class project2
+{
+   public static void main(String[] args) //Project 2: Old Lady, Cj Karinen
+   {
+      verse1();
+      System.out.println();
+      verse2();
+      System.out.println();
+      verse3();
+      System.out.println();
+      verse4();
+      System.out.println();
+      verse5();
+      System.out.println();
+      refrain();
+   
+   
+   
+   
+   
+   
+   
+   }
+   public static void verse1()
+   {
+      System.out.println("There was an old lady who swallowed a fly.");
+      System.out.println("I don't know why she swallowed that fly,");
+      System.out.println("Perhaps she'll die.");
+   
+   
+   }
+   public static void verse2()
+   {
+      System.out.println("There was an old lady who swallowed a spider,");
+      System.out.println("That wriggled and iggled and jiggled inside her.");
+      System.out.println("She swallowed the spider to catch the fly,");
+      System.out.println("I don't know why she swallowed that fly,");
+      System.out.println("Perhaps she'll die.");
+   
+   
+   }
+   public static void verse3()
+   {
+      System.out.println("There was an old lady who swallowed a bird,");
+      System.out.println("How absurd to swallow a bird");
+      System.out.println("She swallowed the bird to catch the spider,");
+      System.out.println("She swallowed the spider to catch the fly,");
+      System.out.println("I don't know why she swallowed that fly,");
+      System.out.println("Perhaps she'll die.");
+   
+   
+   }
+   public static void verse4()
+   {
+      System.out.println("There was an old lady who swallowed a cat,");
+      System.out.println("Imagine that to swallow to a cat.");
+      System.out.println("She swallowed the cat to catch the bird,");
+      System.out.println("She swallowed the bird to catch the spider,");
+      System.out.println("She swallowed the spider to catch the fly,");
+      System.out.println("I don't know why she swallowed that fly,");
+      System.out.println("Perhaps she'll die.");
+   
+   
+   }
+   public static void verse5()
+   {
+      System.out.println("There was an old lady who swallowed a dog,");
+      System.out.println("What a hog to swallow a dog.");
+      System.out.println("She swallowed the dog to catch the cat,");
+      System.out.println("She swallowed the cat to catch the bird,");
+      System.out.println("She swallowed the bird to catch the spider,");
+      System.out.println("She swallowed the spider to catch the fly,");
+      System.out.println("I don't know why she swallowed that fly,");
+      System.out.println("Perhaps she'll die.");
+   
+   
+   }
+   public static void refrain()
+   {
+      System.out.println("There was an old lady who swallowed a horse,");
+      System.out.println("She died of course.");
+         
+   
+   }
+
+
+
+   
+
+
+
+
+
+
+
+}
